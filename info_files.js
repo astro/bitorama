320e6ad8a857104b761d1ae8d428679d185cc693
Astro <astro@spaceboyz.net>
2014-03-06 00:26:36 +0100

    info_files: safety

--- info_files.js
+++ info_files.js
@@ -2,18 +2,23 @@ module.exports = infoToFiles;
 
 function infoToFiles(info, infoHash) {
     var name = Buffer.isBuffer(info.name) ?
-        info.name.toString() :
-        infoHash;
+        info.name.toString() : "";
+    if (!isValidPath(name)) {
+        name = infoHash;
+    }
+
     if (typeof info.length === 'number') {
+        /* Single-file torrent */
         return [{
             path: [name],
             length: info.length
         }];
     } else if (Array.isArray(info.files)) {
+        /* Multi-file torrent */
         return info.files.map(function(file) {
             var path = file.path.map(function(buf) {
                 return buf.toString();
-            });
+            }).filter(isValidPath);
             return {
                 path: [name].concat(path),
                 length: file.length
@@ -23,3 +28,7 @@ function infoToFiles(info, infoHash) {
         throw new Error("No files in info");
     }
 };
+
+function isValidPath(s) {
+    return s != "" && s != "." && s != "..";
+}
