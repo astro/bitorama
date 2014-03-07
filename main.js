e03a4540dc1cfc7aa233f1dd2c1824118e68d2bd
Astro <astro@spaceboyz.net>
2014-03-07 23:05:57 +0100

    use magnet-uri 1.1.0

--- main.js
+++ main.js
@@ -12,9 +12,8 @@ process.argv.slice(2).forEach(function(url) {
     if (/^magnet:/.test(url)) {
         var parsed = Magnet(url)
         console.log(parsed)
-        if (parsed.btih) {
-            var infoHash = parsed.btih;
-            var ctx = new TorrentContext(infoHash);
+        if (parsed.infoHash) {
+            var ctx = new TorrentContext(parsed.infoHash);
 
 
             parsed.tr.forEach(function(t) {
